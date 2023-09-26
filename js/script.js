63913081706c9bcc9f23577b100e2b20b365ead6
Dryzin <endrioemanuel11@gmail.com>
2023-09-26 13:15:04 -0300

    correção estrutural do js e add border nos banner

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const slideContainer = document.querySelector(".slide-container");
   const carouselSlide = document.querySelector(".carousel-slide");
   const images = document.querySelectorAll(".carousel-slide img");
-
-  const interval = 6000; // Intervalo de 10 segundos entre as imagens
-
+  const interval = 6000; // Intervalo de 6 segundos entre as imagens
   let currentIndex = 0;
 
   function nextSlide() {
@@ -24,44 +22,38 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(nextSlide, interval); // Alterna automaticamente entre as imagens
 
   updateSlide(); // Garante que a primeira imagem seja exibida corretamente
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-    const prevButton = document.querySelector(".prev-button");
-    const nextButton = document.querySelector(".next-button");
-    const carouselContainer = document.querySelector(".carousel-container");
-    const productList = document.querySelector(".product-list");
-  
-    let currentIndex = 0;
-    const itemsPerPage = 4;
-  
-    prevButton.addEventListener("click", function () {
-      if (currentIndex > 0) {
-        currentIndex -= itemsPerPage;
-        updateCarousel();
-      }
-    });
-  
-    nextButton.addEventListener("click", function () {
-      if (currentIndex < productList.children.length - itemsPerPage) {
-        currentIndex += itemsPerPage;
-        updateCarousel();
-      }
-    });
-  
-    function updateCarousel() {
-      const translateX = -currentIndex * (100 / itemsPerPage) + "%";
-      productList.style.transform = `translateX(${translateX})`;
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+  const carouselContainer = document.querySelector(".carousel-container");
+  const productList = document.querySelector(".product-list");
+
+  const itemsPerPage = 4;
+
+  prevButton.addEventListener("click", function () {
+    if (currentIndex > 0) {
+      currentIndex -= itemsPerPage;
+      updateCarousel();
     }
-
-    updateSlide();
-
-    nextButton.addEventListener("click", nextSlide);
-
   });
 
-  // Função para mostrar o contador de produtos e o botão "Adicionar"
-  function showQuantityAndBuyButton(product) {
+  nextButton.addEventListener("click", function () {
+    if (currentIndex < productList.children.length - itemsPerPage) {
+      currentIndex += itemsPerPage;
+      updateCarousel();
+    }
+  });
+
+  function updateCarousel() {
+    const translateX = -currentIndex * (100 / itemsPerPage) + "%";
+    productList.style.transform = `translateX(${translateX})`;
+  }
+
+  nextButton.addEventListener("click", nextSlide);
+});
+
+// Função para mostrar o contador de produtos e o botão "Adicionar"
+function showQuantityAndBuyButton(product) {
   const quantityElement = product.querySelector('.product-quantity');
   const buyButton = product.querySelector('.btn-buy');
   quantityElement.style.display = 'flex';
@@ -96,7 +88,7 @@ function incrementQuantity(button) {
   quantityValue.textContent = quantity;
 }
 
-// Função para decrementar a quantidade de produtos (com mínimo de 0)
+// Função para decrementar a quantidade de produtos
 function decrementQuantity(button) {
   const quantityValue = button.parentNode.querySelector('.quantity-value');
   let quantity = parseInt(quantityValue.textContent);
@@ -117,28 +109,23 @@ productItems.forEach((productItem) => {
   });
 });
 
-
-//SLIDE BOX
-$(function(){
-    
-      $('.bxCarrosel').bxSlider({
-        
-        mode: 'horizontal',
-        swipeThreshold:true,
-        touchEnabled:true,
-        oneToOneTouch:true,
-        auto: true,
-        controls:false,
-        infiniteLoop: true,
-        autoHover:true,
-        slideWidth:500,
-        //slideMargin:0,
-        responsive:true,
-        minSlides: 4,
-        maxSlides: 4,
-        shrinkItems:false,
-        speed:2000
-      
-      });
-
-}); 
+// SLIDE BOX
+$(function () {
+  $('.bxCarrosel').bxSlider({
+    mode: 'horizontal',
+    swipeThreshold: true,
+    touchEnabled: true,
+    oneToOneTouch: true,
+    auto: true,
+    controls: false,
+    infiniteLoop: true,
+    autoHover: true,
+    slideWidth: 500,
+    // slideMargin: 0,
+    responsive: true,
+    minSlides: 4,
+    maxSlides: 4,
+    shrinkItems: false,
+    speed: 2000
+  });
+});
